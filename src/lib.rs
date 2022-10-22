4de12c8d9839026e4b2f55eaeea91c30e3b5dfea
Andreas Steinkellner <asteinkellner1@gmail.com>
2022-10-22 22:43:30 +0200

    chore: introduce index buffer

--- src/lib.rs
+++ src/lib.rs
@@ -52,18 +52,30 @@ impl Vertex {
 // front_face: wgpu::FrontFace::Ccw, we need to list these vertices in clockwise order
 const VERTICES: &[Vertex] = &[
     Vertex {
-        position: [0.0, 0.5, 0.0],
-        color: [1.0, 0.0, 0.0],
-    },
+        position: [-0.0868241, 0.49240386, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, // A
     Vertex {
-        position: [-0.5, -0.5, 0.0],
-        color: [0.0, 1.0, 0.0],
-    },
+        position: [-0.49513406, 0.06958647, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, // B
     Vertex {
-        position: [0.5, -0.5, 0.0],
-        color: [1.0, 0.0, 1.0],
-    },
+        position: [-0.21918549, -0.44939706, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, // C
+    Vertex {
+        position: [0.35966998, -0.3473291, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, // D
+    Vertex {
+        position: [0.44147372, 0.2347359, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, // E
 ];
+
+// Indices stores the mapping of vertices so we don't have to repeat
+// defining shared points multiple times
+const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4];
 
 struct State {
     surface: wgpu::Surface,
@@ -75,6 +87,8 @@ struct State {
     render_pipeline: wgpu::RenderPipeline,
     vertex_buffer: wgpu::Buffer,
     num_vertices: u32,
+    index_buffer: wgpu::Buffer,
+    num_indices: u32,
 }
 
 impl State {
@@ -187,6 +201,13 @@ impl State {
             usage: wgpu::BufferUsages::VERTEX,
         });
 
+        let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Index Buffer"),
+            contents: bytemuck::cast_slice(INDICES),
+            usage: wgpu::BufferUsages::INDEX,
+        });
+        let num_indices = INDICES.len() as u32;
+
         let num_vertices = VERTICES.len() as u32;
         let background_color = wgpu::Color::BLACK;
         Self {
@@ -199,6 +220,8 @@ impl State {
             render_pipeline,
             vertex_buffer,
             num_vertices,
+            index_buffer,
+            num_indices,
         }
     }
 
@@ -267,7 +290,10 @@ impl State {
             render_pass.set_pipeline(&self.render_pipeline);
             // We use the entire vertex buffer for this render_pass
             render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-            render_pass.draw(0..self.num_vertices, 0..1); // Draw with 3 vertices in 1 instance
+
+            // Since we're using an index buffer, we need to use `draw_indexed`
+            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+            render_pass.draw_indexed(0..self.num_indices, 0, 0..1); // Draw with 3 vertices in 1 instance
         } // Here we release our borrow on the encoder, so we can finish
 
         self.queue.submit(iter::once(encoder.finish()));
